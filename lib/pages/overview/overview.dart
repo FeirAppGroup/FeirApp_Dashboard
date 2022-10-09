54c2cef35a0472c68f08389e145e2f66dcd33013
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-10-09 12:47:29 -0300

    Cards adicionados a aplicacao

--- lib/pages/overview/overview.dart
+++ lib/pages/overview/overview.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import '../../constants/controllers.dart';
+import '../../helpers/responsiveness.dart';
+import '../../widgets/Cards/overview_cards_large.dart';
+import '../../widgets/Cards/overview_cards_medium.dart';
+import '../../widgets/Cards/overview_cards_small.dart';
 import '../../widgets/Text/custom_text.dart';
 
 class OverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CustomText(
-        text: "OverView",
-      ),
+    return Column(
+      children: [
+        Obx(
+          () => Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: ResponsiveWidget.isSmallScreen(context) ? 56 : 6,
+                ),
+                child: CustomText(
+                  text: menuController.activeItem.value,
+                  size: 24,
+                  weight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView(
+            children: [
+              if (ResponsiveWidget.isLargeScreen(context) || ResponsiveWidget.isMediumScreen(context))
+                if (ResponsiveWidget.isCustomSize(context)) OverviewCardsMediumScreen() else OverviewCardsLargeScreen()
+              else
+                OverviewCardsSmallScreen()
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
